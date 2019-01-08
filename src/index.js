8518cdeba92e0c6dbe1020da6ec7ac6537028949
John Lin <jlin1674@gmail.com>
2019-01-08 14:03:43 -0500

    general structure

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Square extends React.Component {
+	render() {
+		return null;
+	}
+}
+
+class Board extends React.Component {
+	render() {
+		return null;
+	}
+}
+
+class Game extends React.Component {
+	render() {
+		return null;
+	}
+}
+
+ReactDOM.render(
+	<Game />,
+	document.getElementById('root')
+);
+
