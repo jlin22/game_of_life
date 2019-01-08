8c89e4d23877aeac05aef11503b9a7ce70ab2edd
John Lin <jlin1674@gmail.com>
2019-01-08 14:14:23 -0500

    squares state

--- src/index.js
+++ src/index.js
@@ -3,24 +3,49 @@ import ReactDOM from 'react-dom';
 
 class Square extends React.Component {
 	render() {
-		return null;
+		return (
+			<button className="square">
+			</button>
+		);
 	}
 }
 
+//Board manages the squares
 class Board extends React.Component {
 	render() {
-		return null;
+		const squareList = this.props.squares;
+		return <Square />;
 	}
 }
 
+//Game manages the game logic
 class Game extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			squares: this.createBoard()
+		}
+	}
+
+	createBoard() {
+		const board = [];
+		for (var r = 0; r < parseInt(this.props.rows); r++) {
+			board.push([]);
+			for (var c = 0; c < parseInt(this.props.cols); c++) {
+				board[r].push(false);	
+			}
+		}
+		return board;
+	}
+
 	render() {
-		return null;
+		console.log(this.state.squares);
+		return <Board />;
 	}
 }
 
 ReactDOM.render(
-	<Game />,
+	<Game rows="20" cols="20"/>,
 	document.getElementById('root')
 );
 
